201b18561f71fd1e18e257d0fbc0fa4f7bddd752
Lewis Mewis <lewdmilldev@gmail.com>
2022-06-03 23:22:25 +0100

    Some tidying

--- LinearRegression/ecommerce.py
+++ LinearRegression/ecommerce.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", None)
+pd.set_option("display.max_colwidth", None)
 
 customers = pd.read_csv("Ecommerce Customers")
 
