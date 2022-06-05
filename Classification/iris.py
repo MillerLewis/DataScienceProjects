ff8cc400deb647742bb64a9e3e23593829d53a37
Lewis Mewis <lewdmilldev@gmail.com>
2022-06-05 13:52:30 +0100

    Refactoring iris data

--- Classification/iris.py
+++ Classification/iris.py
@@ -30,6 +30,7 @@ X = iris.drop("species", axis=1)
 y = iris["species"]
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.3, random_state=101)
 
+# FITTING
 svc = SVC()
 svc.fit(X_train, y_train)
 pred = svc.predict(X_test)
